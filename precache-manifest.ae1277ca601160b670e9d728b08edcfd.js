4f194f4f6ee9ef593dded4d61e59b3a96d05b05f
slavka5212 <slavka5212@gmail.com>
2020-04-07 16:56:42 +0200

    Updates

---
+++ precache-manifest.ae1277ca601160b670e9d728b08edcfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd40d9c3030ce76b557387e8c38d859",
+    "url": "/fantastic-cards/index.html"
+  },
+  {
+    "revision": "e773f6f1e00a0845743e",
+    "url": "/fantastic-cards/static/css/main.d5c347f4.chunk.css"
+  },
+  {
+    "revision": "44432d24331bd47cbaa3",
+    "url": "/fantastic-cards/static/js/2.31d64e6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fantastic-cards/static/js/2.31d64e6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e773f6f1e00a0845743e",
+    "url": "/fantastic-cards/static/js/main.260ca7f0.chunk.js"
+  },
+  {
+    "revision": "c4b14226bd4fbd569027",
+    "url": "/fantastic-cards/static/js/runtime-main.8bb2e161.js"
+  }
+]);
